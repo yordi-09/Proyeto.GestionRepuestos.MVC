2a0669d2337dee94f2b2569958757d1421ed35bd
Yordi Elizondo Montes <yelizondo@datasoft.co.cr>
2025-06-26 18:24:01 -0600

    Agregar archivos de proyecto.

---
+++ Proyeto.GestionRepuestos.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proyeto.GestionRepuestos.MVC.Startup))]
+namespace Proyeto.GestionRepuestos.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

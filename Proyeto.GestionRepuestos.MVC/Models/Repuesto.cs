8610980aa12f629f79ceeba23672cccfcb45bc3a
Yordi Elizondo Montes <yelizondo@datasoft.co.cr>
2025-07-19 16:26:51 -0600

    Mejoras el UI del sistema

--- Proyeto.GestionRepuestos.MVC/Models/Repuesto.cs
+++ Proyeto.GestionRepuestos.MVC/Models/Repuesto.cs
@@ -9,16 +9,17 @@ namespace Proyeto.GestionRepuestos.MVC.Models
 
         [Required(ErrorMessage = "El nombre es obligatorio.")]
         [StringLength(100)]
-        [Display(Name = "Nombre Repuesto")]
+        [Display(Name = "Nombre repuesto")]
         public string Nombre { get; set; }
 
         [Required(ErrorMessage = "La descripción es obligatoria.")]
         [StringLength(250)]
+        [Display(Name = "Descripción del repuesto")]
         public string Descripcion { get; set; }
 
         [Required(ErrorMessage = "La cantidad es obligatoria.")]
         [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser mayor a 0.")]
-        [Display(Name = "Cantidad Disponible")]
+        [Display(Name = "Cantidad disponible")]
         public int CantidadDisponible { get; set; }
 
         [Required(ErrorMessage = "El precio es obligatorio.")]
@@ -26,7 +27,7 @@ namespace Proyeto.GestionRepuestos.MVC.Models
         [Display(Name = "Precio")]
         public decimal Precio { get; set; }
 
-        [Display(Name = "Fecha de Ingreso")]
+        [Display(Name = "Fecha de ingreso")]
         public DateTime FechaIngreso { get; set; } = DateTime.Now;
     }
 }
